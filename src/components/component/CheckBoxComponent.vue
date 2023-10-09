476d494c9b04f95bc2966ac04928ec9b2b77e8fb
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2023-10-10 02:45:01 +0900

    duplicated router

---
+++ src/components/component/CheckBoxComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <h2>체크박스 목록</h2>
+        <div v-for="item in items" :key="item.id">
+          <input type="checkbox" v-model="item.checked">
+          {{ item.label }}
+    </div>
+      <p>선택된 항목: {{ selectedItems }}</p>
+    </div>
+</template>
+      
+<script>
+export default {
+    data() {
+     return {
+        items: [
+        { id: 1, label: '항목 1', checked: false },
+        { id: 2, label: '항목 2', checked: false },
+        { id: 3, label: '항목 3', checked: false },
+        // 필요한 만큼 항목을 추가할 수 있습니다.
+        ],       
+     };
+    },
+    computed: {
+     selectedItems() {
+      return this.items.filter(item => item.checked).map(item => item.label);
+     },
+    },
+};
+</script>
+      
+<style>
+  
+</style>
--- src/components/page/GenrePage.vue
+++ src/components/page/GenrePage.vue
@@ -1,13 +1,7 @@
 <template>
   <div>
-    <h2>체크박스 목록</h2>
-      <div v-for="item in items" :key="item.id">
-        <input type="checkbox" v-model="item.checked">
-        {{ item.label }}
+    <CheckBoxComponent/>
   </div>
-    <p>선택된 항목: {{ selectedItems }}</p>
-  </div>
-
   <div>
     <SearchComponent/>
   </div>
@@ -15,26 +9,14 @@
     
   <script>
   import SearchComponent from '../component/SearchComponent.vue'; // PageCarousel 컴포넌트 import
+  import CheckBoxComponent from '../component/CheckBoxComponent.vue'; // PageCarousel 컴포넌트 import
+
 
   export default {
       components: {
         SearchComponent,
+        CheckBoxComponent,
       },
-      data() {
-        return {
-          items: [
-          { id: 1, label: '항목 1', checked: false },
-          { id: 2, label: '항목 2', checked: false },
-          { id: 3, label: '항목 3', checked: false },
-          // 필요한 만큼 항목을 추가할 수 있습니다.
-        ],       
-        };
-      },
-      computed: {
-      selectedItems() {
-        return this.items.filter(item => item.checked).map(item => item.label);
-      },
-    },
   };
   </script>
     
