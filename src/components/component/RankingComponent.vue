f786d440c42b225580fe0390614d43801faaac40
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2023-10-11 05:36:53 +0900

    add pagenation

--- src/components/component/RankingComponent.vue
+++ src/components/component/RankingComponent.vue
@@ -1,19 +1,28 @@
 <template>
-    <div class="ranking">
-      <h1>
-        <font-awesome-icon :icon="['fas', 'crown']" />
-        랭킹순
-      </h1>
-      <div class="grid-container">
-        <div v-for="(novel, index) in novels" :key="index" class="grid-item">
-          <div class="grid-item-content">
-            <img :src="novel.imageUrl" alt="Novel Image" class="image" />
-            <p class="title">{{ novel.title }}</p>
-          </div>
+  <div class="ranking">
+    <h1>
+      <font-awesome-icon :icon="['fas', 'crown']" />
+      랭킹순
+    </h1>
+    <div class="grid-container">
+      <div v-for="(novel, index) in paginatedNovels" :key="index" class="grid-item">
+        <div class="grid-item-content">
+          <img :src="novel.imageUrl" alt="Novel Image" class="image" />
+          <p class="title">{{ novel.title }}</p>
         </div>
       </div>
     </div>
-  </template>
+    <div class="pagination">
+      <div class="page-bar">
+        <button @click="prevPage" :disabled="currentPage === 1">이전</button>
+        <div class="page" v-for="page in totalPages" :key="page">
+          <button @click="goToPage(page)" :class="{ active: currentPage === page }">{{ page }}</button>
+        </div>
+        <button @click="nextPage" :disabled="currentPage === totalPages">다음</button>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
@@ -24,43 +33,123 @@ export default {
         { title: '소설 2', imageUrl: 'https://comicthumb-phinf.pstatic.net/20160216_216/pocket_14556178273891qpTh_JPEG/h300.jpg?type=m600x314' },
         { title: '소설 3', imageUrl: 'https://comicthumb-phinf.pstatic.net/20230615_64/pocket_1686819221240B5JFh_JPEG/cover_.jpg?type=m600x314' },
         { title: '소설 4', imageUrl: 'https://comicthumb-phinf.pstatic.net/20150130_144/pocket_1422603514525Mxe6h_JPEG/%BC%F6%B6%F3%BF%D5_%BD%C5%B1%D4_%C7%A5%C1%F6_%C0%CE%C5%B8%C0%CC%C6%B2.JPG?type=m600x314' },
+        { title: '소설 5', imageUrl: 'https://comicthumb-phinf.pstatic.net/20141010_110/pocket_1412915784499GktmY_JPEG/cloud.jpg?type=m600x314' },
+        { title: '소설 6', imageUrl: 'https://comicthumb-phinf.pstatic.net/20160216_216/pocket_14556178273891qpTh_JPEG/h300.jpg?type=m600x314' },
+        { title: '소설 7', imageUrl: 'https://comicthumb-phinf.pstatic.net/20230615_64/pocket_1686819221240B5JFh_JPEG/cover_.jpg?type=m600x314' },
+        { title: '소설 8', imageUrl: 'https://comicthumb-phinf.pstatic.net/20150130_144/pocket_1422603514525Mxe6h_JPEG/%BC%F6%B6%F3%BF%D5_%BD%C5%B1%D4_%C7%A5%C1%F6_%C0%CE%C5%B8%C0%CC%C6%B2.JPG?type=m600x314' },
+        { title: '소설 9', imageUrl: 'https://comicthumb-phinf.pstatic.net/20141010_110/pocket_1412915784499GktmY_JPEG/cloud.jpg?type=m600x314' },
+        { title: '소설 10', imageUrl: 'https://comicthumb-phinf.pstatic.net/20160216_216/pocket_14556178273891qpTh_JPEG/h300.jpg?type=m600x314' },
+        { title: '소설 11', imageUrl: 'https://comicthumb-phinf.pstatic.net/20230615_64/pocket_1686819221240B5JFh_JPEG/cover_.jpg?type=m600x314' },
+        { title: '소설 12', imageUrl: 'https://comicthumb-phinf.pstatic.net/20150130_144/pocket_1422603514525Mxe6h_JPEG/%BC%F6%B6%F3%BF%D5_%BD%C5%B1%D4_%C7%A5%C1%F6_%C0%CE%C5%B8%C0%CC%C6%B2.JPG?type=m600x314' },
         // 필요한 만큼 소설 데이터를 추가합니다.
       ],
+      currentPage: 1,
+      itemsPerPage: 4, // 페이지당 아이템 수를 4개로 설정
     };
+  },
+  computed: {
+    paginatedNovels() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      return this.novels.slice(startIndex, endIndex);
+    },
+    totalPages() {
+      return Math.ceil(this.novels.length / this.itemsPerPage);
+    },
+  },
+  methods: {
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage -= 1;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage += 1;
+      }
+    },
+    goToPage(page) {
+      this.currentPage = page;
+    },
   },
 };
 </script>
-  
-  <style scoped>
-  .ranking {
-    text-align: left;
-    margin: 30px;
-  }
-  
-  .grid-container {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr); /* 4x4 그리드 생성 */
-    gap: 10px; /* 그리드 아이템 간의 간격 설정 */
-  }
-  
-  .grid-item {
-    border: 1px solid #ccc; /* 그리드 아이템에 테두리 추가 */
-    padding: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .grid-item-content {
-    text-align: center;
-  }
-  
-  .image {
-    max-width: 100%; /* 이미지를 그리드 아이템 내에 맞춥니다. */
-  }
-  
-  .title {
-    margin-top: 5px;
-  }
-  </style>
-  
+
+<style scoped>
+.ranking {
+  text-align: left;
+  margin: 30px;
+}
+
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+}
+
+.grid-item {
+  border: 1px solid #ccc;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.grid-item-content {
+  text-align: center;
+}
+
+.image {
+  max-width: 100%;
+}
+
+.title {
+  margin-top: 5px;
+}
+
+.pagination {
+  text-align: center;
+  margin-top: 20px;
+}
+
+.pagination button {
+  background-color: red;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+.pagination button:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
+}
+
+.page-bar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.page {
+  margin: 0 5px;
+}
+
+.page button {
+  background-color: transparent;
+  border: 1px solid #ccc;
+  color: #333;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+.page button.active {
+  background-color: red;
+  color: white;
+  border: 1px;
+}
+
+.page-bar button {
+  margin: 0 10px;
+}
+</style>
