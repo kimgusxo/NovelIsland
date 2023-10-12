039017cb420435c8d274c9ff4d37cefea7959533
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2023-10-12 15:06:26 +0900

    add axios

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
 import router from './router' // Vue Router를 import
 import store from './store'; // Vuex Store를 import
+import axios from 'axios'; // Vue axios를 import 
 
 library.add(fas)
 
 createApp(App)
   .use(router) // Vue Router를 앱에 등록
   .use(store)
+  .provide('$axios', axios)
   .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app')
