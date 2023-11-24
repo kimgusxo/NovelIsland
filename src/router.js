fc8d3e27bc6ed46f8b0df5850b9000f7741d7356
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2023-11-25 01:27:26 +0900

    router.js update

--- src/router.js
+++ src/router.js
@@ -14,7 +14,7 @@ import InfomationPage from './components/page/InfomationPage'
 import BookMarkPage from './components/page/BookMarkPage'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/NovelIsland-FrontEnd/'),
   routes: [
     {
       path: '/',
